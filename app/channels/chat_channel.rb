771c9300638b2a277270c911f5672b1843d26308
Dhruv Bhakta <dhruvbbhakta@gmail.com>
2021-04-18 14:32:10 +0530

    Private chat support

--- app/channels/chat_channel.rb
+++ app/channels/chat_channel.rb
@@ -2,11 +2,14 @@ class ChatChannel < ApplicationCable::Channel
   # Called when the consumer has successfully
   # become a subscriber to this channel.
   def subscribed
-    stream_from 'public_chat_room'
+    stream_from "chat_room-#{params[:start_chat_room]}"
   end
 
   # recive data when client send data
   def receive(data)
-    ActionCable.server.broadcast('public_chat_room', data)
+    puts("data.class.name = #{data.class.name}")
+    puts("data['room'] = #{data['room']}")
+
+    ActionCable.server.broadcast("chat_room-#{data['room_name']}", data)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
+  devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get "chat/enter_name", to: "chat#enter_name", as: :chat_enter_name
   get "chat/enter_in_room", to: "chat#enter_in_room", as: :chat_enter_in_room
-  get "chat/room", to: "chat#room", as: :chat_room
+  get "chats", to: "chat#index"
 end
